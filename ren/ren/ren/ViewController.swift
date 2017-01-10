d5e1ce730d38eff90a6901d93ccdb29be69e2d3e
Theron Patrick <theronbpatrick@gmail.com>
2017-01-10 07:20:01 -0500

    Set up grid, colors, sounds

--- ren/ren/ren/ViewController.swift
+++ ren/ren/ren/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
+
+internal var audioArray: Array = [
+    "bass 1",
+    "bass 2",
+    "kick1",
+    "rip 1",
+    "shake 1",
+    "snare 1"
+]
+
+internal var playerArray = [AVAudioPlayer]()
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 0...audioArray.count - 1 {
+            let newPlayer: AVAudioPlayer!;
+            let url = Bundle.main.url(forResource: audioArray[i], withExtension: "wav")!
+            print(url)
+            do {
+                newPlayer = try AVAudioPlayer(contentsOf: url)
+                print(newPlayer)
+                playerArray.append(newPlayer)
+                playerArray[i].prepareToPlay()
+            } catch let error as NSError {
+                print(error.description)
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +48,22 @@ class ViewController: UIViewController {
     }
 
     @IBAction func padDown(_ sender: AnyObject) {
-        print("lel")
+        // Tag starts at 1 because 0 indicates no tag assigned
+        let index = sender.tag - 1
+        
+        (sender as! UIButton).backgroundColor = UIColor(red:1.00, green:0.99, blue:0.98, alpha:1.0)
+        
+        if (index > -1 && index < audioArray.count) {
+            let player = playerArray[index];
+            player.stop()
+            player.currentTime = 0
+            player.play()
+        }
+        
+    }
+    @IBAction func padTouchUp(_ sender: AnyObject) {
+        print("cool")
+        (sender as! UIButton).backgroundColor = UIColor(netHex: 0x96A6B8)
     }
 
 }
